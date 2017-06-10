bb2cb23f33a350ee44cf145e7c75525b8a833a06
Administrador <Administrador@10.10.3.4>
2017-06-10 11:20:44 -0300

    alteracao_cliente_controler

--- conexaoBanco/src/conexaobanco/controler/ClienteControler.java
+++ conexaoBanco/src/conexaobanco/controler/ClienteControler.java
@@ -4,18 +4,22 @@ package conexaobanco.controler;
 import conexaobanco.Conexao;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 public class ClienteControler {
     
-    public boolean inserir() throws SQLException{
+    public boolean inserir(int codigo , String nome ) {
        
-        String sql = "INSERT INTO cliente(nome,codigo) VALUES ('Zolet',7)";
-        PreparedStatement ps;
+        String sql = "INSERT INTO cliente(codigo,nome) VALUES (?,?)";
+        PreparedStatement ps;//esta preparando o comando 
         try{
-                ps = Conexao.getConexao().prepareStatement(sql);
-                ps.execute();
+                ps = Conexao.getConexao().prepareStatement(sql);//esta preparando para excutar o SQL pra nos da false 
+                ps.setInt(1, codigo);
+                ps.setString(2, nome);
+                ps.execute();//excute o comado SQL
                 return true;
         }catch (SQLException | ClassNotFoundException ex){
                 Logger.getLogger(ClienteControler.class.getName()).log(Level.SEVERE,null, ex);
@@ -23,10 +27,15 @@ public class ClienteControler {
         return false;
     }
     
-         public static void main(String[] args) throws SQLException {
+         public static void main(String[] args) {
         ClienteControler  p = new ClienteControler ();
-            p.inserir();
-             System.out.println(""+p);
+        Scanner l = new Scanner(System.in);
+             for (int i = 0; i < 3; i++) {
+             String f = JOptionPane.showInputDialog("DIGITE O NOME");
+            int n =l.nextInt() ;
+            p.inserir(n, f);
+           }
+            
          }
 
 }
