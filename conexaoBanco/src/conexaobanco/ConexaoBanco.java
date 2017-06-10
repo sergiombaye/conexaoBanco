cc7f7c469212846795e43505eeb310f603de15b0
Administrador <Administrador@10.10.3.4>
2017-06-10 09:03:24 -0300

    OLA_MUNDO

--- conexaoBanco/src/conexaobanco/ConexaoBanco.java
+++ conexaoBanco/src/conexaobanco/ConexaoBanco.java
@@ -3,7 +3,7 @@ package conexaobanco;
 public class ConexaoBanco {
   
     public static void main(String[] args) {
-        System.out.println("OLA MUNDO"); 
+        System.out.println("ola mundo");
     }
     
 }
